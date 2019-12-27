595fee5125799fa83c7eaf1d6b80868b7561dbfa
guoyujie <296963166@qq.com>
2019-12-27 15:38:29 +0800

    a

--- entry/index.js
+++ entry/index.js
@@ -1,7 +1,6 @@
-'use strict'
-
-const priter= require("qtnode-middleware-console");
-const path = require("path");
+'use strict';
+const priter= require('qtnode-middleware-console');
+const path = require('path');
 
 const express = require('express');
 const webpack = require('webpack');
@@ -13,33 +12,35 @@ const app = express();
 
 module.exports = function (args) {
     let opts = Object.assign({}, args);
-    const packagejson = require(path.resolve(path.resolve(opts.rootDir, 'package.json')));
+    // const packagejson = require(path.resolve(path.resolve(opts.rootDir, 'package.json')));
     const prodconfig = require(path.resolve(path.resolve(opts.rootDir, 'wpconf/dev.js')));
     const compiler = webpack(prodconfig);
-    return async function (next) {
-        priter.info("start dev");
+    let entry = prodconfig.entry;
 
-        // 使用webpack-dev-middleware中间件
+    for (let {key} of entry ) {
+        entry[key] = ['webpack-hot-middleware/client?noInfo=true&reload=true', entry[key]];
+
+    }
+    prodconfig.entry = entry;
+    return async function (next) {
+        priter.info('start dev');
+
         app.use(webpackDevMiddleware(compiler, {
             publicPath: prodconfig.output.publicPath,
             info: false,
             noInfo: true
         }));
 
-        // 使用webpack-hot-middleware中间件，配置在console台输出日志
         app.use(webpackHotMiddleware(compiler, {
-            log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000}));
-        // app.use(express.static(config.output.path))
+            log: priter.data, path: '/__webpack_hmr', heartbeat: 10 * 1000}));
 
-        // 使用静态资源目录，才能访问到/dist/idndex.html
-        app.use(express.static(prodconfig.output.path))
+        app.use(express.static(prodconfig.output.path));
 
-        // Serve the files on port 3000.
         const server = app.listen( ()=> {
-            priter.tip("==================================");
+            priter.tip('==================================');
             priter.info(`devserver listening: http://localhost:${server.address()['port']}` );
-            priter.tip("==================================");
-        })
+            priter.tip('==================================');
+        });
 
         next();
 
